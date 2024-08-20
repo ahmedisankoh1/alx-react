141321462d066adcf37cdadaba98532b0e9419b4
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2024-08-20 01:32:15 +0000

    redoing task-1

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -4,10 +4,10 @@ import _ from 'lodash';
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count">Here</p>');
+$('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-let conunt = 0;
+let count = 0;
 
 function updateCounter() {
   count++;
